30c834520c03ebd53571c6ae99f305a6bd1eb8a4
garritfra <garritfranke@gmail.com>
2018-10-23 20:47:15 +0200

    Refactor environment variables

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,6 @@ import Login from "./Login/Login";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "../styles/app.scss";
 
-// Initialize Firebase
-const config = {
-  apiKey: "AIzaSyBvYP_HXbReuOgYcd7j9ojMwvClIlrh4F4",
-  authDomain: "ausbildungs-planer.firebaseapp.com",
-  databaseURL: "https://ausbildungs-planer.firebaseio.com",
-  projectId: "ausbildungs-planer",
-  storageBucket: "",
-  messagingSenderId: "864868139007"
-};
-firebase.initializeApp(config);
-
 export default class App extends Component {
   constructor(props) {
     super(props);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "../src/components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import firebase from "firebase";
+
+require("dotenv").load();
+
+// Initialize Firebase
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "ausbildungs-planer.firebaseapp.com",
+  databaseURL: "https://ausbildungs-planer.firebaseio.com",
+  projectId: "ausbildungs-planer",
+  storageBucket: "",
+  messagingSenderId: "864868139007"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.querySelector("#root"));
