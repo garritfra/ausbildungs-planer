4b16edc6826a27b24316e9c24bb4ec0455d46c7b
garritfra <garritfranke@gmail.com>
2018-10-23 21:20:12 +0200

    Typescriptify Index.js

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../styles/app.scss";
 import Header from "./Header/Header";
 import Login from "./Login/Login";
 import Main from "./Main/Main";
 
-export default class App extends Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import firebase from "firebase";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../src/components/App";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/App";
 
 require("dotenv").load();
 
