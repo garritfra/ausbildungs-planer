a0bc675f4b141319bec30530e7c3ba6f075fcf01
garritfra <garritfranke@gmail.com>
2018-10-27 14:59:39 +0200

    Update fields in database

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,163 @@
 import React from "react";
+import { Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import firebase from "firebase";
 
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      currentUser: null,
+      name: "",
+      betrieb: "",
+      abteilung: "",
+      projekt: "",
+      ausbilder: "",
+      ausbildungsanfang: "",
+      ausbildungsende: ""
+    };
+    this.userRef = null;
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      this.fetchData();
+    });
+  }
+
+  fetchData() {
+    this.userRef = firebase
+      .firestore()
+      .collection("Users")
+      .doc(this.state.currentUser.email.toString());
+
+    this.userRef
+      .get()
+      .then(snapshot => snapshot.data())
+      .then(user => {
+        this.setState({ ...user });
+      });
+  }
+
+  updateData() {
+    this.userRef
+      .update({
+        name: this.state.name,
+        betrieb: this.state.betrieb,
+        abteilung: this.state.abteilung,
+        projekt: this.state.projekt,
+        ausbilder: this.state.ausbilder,
+        ausbildungsanfang: this.state.ausbildungsanfang,
+        ausbildungsende: this.state.ausbildungsende
+      })
+      .then(() => {
+        console.log("Success!");
+      })
+      .catch(err => console.log(err));
+  }
+
+  onNameChanged(event) {
+    this.setState({ name: event.target.value });
+    console.log(this.state);
+  }
+
+  onBetriebChanged(event) {
+    this.setState({ betrieb: event.target.value });
+  }
+
+  onAbteilungChanged(event) {
+    this.setState({ abteilung: event.target.value });
+  }
+
+  onProjektChanged(event) {
+    this.setState({ projekt: event.target.value });
+  }
+
+  onAusbilderChanged(event) {
+    this.setState({ ausbilder: event.target.value });
+  }
+
+  onAusbildungsanfangChanged(event) {
+    this.setState({ ausbildungsanfang: event.target.value });
+  }
+
+  onAusbildungsendeChanged(event) {
+    this.setState({ ausbildungsende: event.target.value });
   }
 
   render() {
-    return <h1>Profile Page</h1>;
+    return (
+      <Container className="mt-5">
+        <Form>
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input
+              type="text"
+              defaultValue="Placeholder"
+              placeholder="Name"
+              onChange={this.onNameChanged.bind(this)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="betrieb">Betrieb</Label>
+            <Input
+              type="text"
+              placeholder="Betrieb"
+              onChange={this.onBetriebChanged.bind(this)}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="abteilung">Abteilung</Label>
+            <Input
+              type="text"
+              placeholder="Abteilung"
+              onChange={this.onAbteilungChanged.bind(this)}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="projekt">Projekt</Label>
+            <Input
+              type="text"
+              placeholder="Projekt"
+              onChange={this.onProjektChanged.bind(this)}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="ausbilder">Ausbilder</Label>
+            <Input
+              type="text"
+              placeholder="Ausbilder"
+              onChange={this.onAusbilderChanged.bind(this)}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="ausbildungsanfang">Anfang der Ausbildung</Label>
+            <Input
+              type="date"
+              placeholder="Anfang der Ausbildung"
+              onChange={this.onAusbildungsanfangChanged.bind(this)}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="ausbildungsende">Ende der Ausbildung</Label>
+            <Input
+              type="date"
+              placeholder="Ende der Ausbildung"
+              onChange={this.onAusbildungsendeChanged.bind(this)}
+            />
+          </FormGroup>
+
+          <Button color="primary" onClick={this.updateData.bind(this)}>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    );
   }
 }
