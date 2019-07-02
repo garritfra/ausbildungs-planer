4f7abed1e816866711fb077778839c05756ab6a1
frankeg <garrit.franke@ckc.de>
2019-07-02 16:18:13 +0200

    Add firebase app

--- src/index.js
+++ src/index.js
@@ -8,15 +8,16 @@ import App from "../src/components/App";
 require("dotenv").load();
 
 // Initialize Firebase
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
+const firebaseConfig = {
+  apiKey: "AIzaSyBvYP_HXbReuOgYcd7j9ojMwvClIlrh4F4",
   authDomain: "ausbildungs-planer.firebaseapp.com",
   databaseURL: "https://ausbildungs-planer.firebaseio.com",
   projectId: "ausbildungs-planer",
-  storageBucket: "",
-  messagingSenderId: "864868139007"
+  storageBucket: "ausbildungs-planer.appspot.com",
+  messagingSenderId: "864868139007",
+  appId: "1:864868139007:web:0846fbe3f19cf91e"
 };
 firebase.initializeApp(config);
-let perf = firebase.performance()
+let perf = firebase.performance();
 
 ReactDOM.render(<App />, document.querySelector("#root"));
