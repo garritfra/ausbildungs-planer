9a9b4f26779fee5d956b38f0eeb23a717016b261
= <garritfranke@gmail.com>
2019-07-06 17:04:26 +0200

    Add featuremanager

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,22 @@ import Header from "./Header/Header";
 import Login from "./Login/Login";
 import Main from "./Main/Main";
 import Profile from "./Profile/Profile";
+import FeatureManager from "../util/FeatureManager";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      timesheetsEnabled: false
+    };
+
+    FeatureManager.instance.timesheetsEnabled.subscribe({
+      next: v => {
+        this.setState({ timesheetsEnabled: v });
+        console.log("Timesheets enabled: " + v);
+      }
+    });
   }
 
   render() {
@@ -18,6 +29,9 @@ export default class App extends Component {
         <div className="app">
           <Header />
           <Route exact path="/" component={Main} />
+          {this.state.timesheetsEnabled && (
+            <Route path="/timesheets" render={() => <h1>Timesheets</h1>} />
+          )}
           <Route path="/login" component={Login} />
           <Route path="/profile" component={Profile} />
         </div>
